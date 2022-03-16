8f8db4a3f2bb036e2233c4edf70609734d008525
Carlos Oliveira <chopcsom@gmail.com>
2022-03-15 22:38:37 -0300

    feat(api): adds refresh token global

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: `http://localhost:3333`,
@@ -9,3 +9,47 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies[`nextauth.token`]}`,
   },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error?.response?.status === 401) {
+      // Refresh token
+      if (error.response?.data?.code === `token.expired`) {
+        cookies = parseCookies();
+
+        const { 'nextauth.refreshToken': refreshToken } = cookies;
+
+        api
+          .post(`/refresh`, { refreshToken })
+          .then((response) => {
+            const { token } = response.data;
+
+            setCookie(undefined, `nextauth.token`, token, {
+              maxAge: 30 * 24 * 60 * 60, // 30 days
+              path: `/`,
+            });
+            setCookie(
+              undefined,
+              `nextauth.refreshToken`,
+              response.data.refreshToken,
+              {
+                maxAge: 30 * 24 * 60 * 60, // 30 days
+                path: `/`,
+              },
+            );
+
+            api.defaults.headers.common[`Authorization`] = `Bearer ${token}`;
+          })
+          .catch((error) => {
+            // TODO: Logout
+            console.log(error);
+          });
+      } else {
+        // Logout
+      }
+    }
+  },
+);
