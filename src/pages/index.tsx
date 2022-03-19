50f0f5c262258c0cee7ceb457e648b8b07085112
Carlos Oliveira <chopcsom@gmail.com>
2022-03-18 22:46:15 -0300

    feat(utils): adds withSSRGuest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { AuthContext } from '@/context/AuthContext';
 import styles from '@/styles/Home.module.css';
 import { GetServerSideProps } from 'next';
 import { parseCookies } from 'nookies';
+import { withSSRGuest } from '@/utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState(``);
@@ -40,19 +41,8 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies[`nextauth.token`]) {
-    return {
-      redirect: {
-        destination: `/dashboard`,
-        permanent: false,
-      },
-    };
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {},
   };
-};
+});
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { withSSRGuest } from './withSSRGuest';
