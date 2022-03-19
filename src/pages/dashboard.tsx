c75c6fcafd38734048cae286104cbb323d18c5ca
Carlos Oliveira <chopcsom@gmail.com>
2022-03-18 23:57:18 -0300

    feat(hook): adds useCan for roles and permissions validations

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,28 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { api } from '@/services/apiClient';
 import { withSSRAuth } from '@/utils';
 import { setupAPIClient } from '@/services/api';
+import { AuthContext } from '@/context/AuthContext';
+import { useCan } from '@/hooks';
 
 export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+
+  const useCanSeeMetrics = useCan({
+    permissions: [`metrics.list`],
+  });
   useEffect(() => {
     api.get(`/me`).then((response) => {
       console.log(response?.data);
     });
   }, []);
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {useCanSeeMetrics && <h1>Metrics</h1>}
+    </>
+  );
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
